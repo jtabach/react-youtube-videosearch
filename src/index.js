f41aafacdc94aac7e067cb16d48eae019db39835
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-22 15:21:21 -0700

    confgiures SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDXrY5YCNGwaUD19FvtiXLCn_BCb0qT5IM';
+
 // Create a new component. This component should produce
 // some HTML
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's genreated HTLM an dput it
 // on the page (in the DOM)
-ReactDOM.render(<App />
+ReactDOM.render(
+  <App />
   ,
   document.querySelector('.container')
 );
