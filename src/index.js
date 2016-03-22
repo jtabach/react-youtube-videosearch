fbf75de369c4481ad3e7bbeec6adf725699d5a39
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-22 14:58:10 -0700

    render App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// Create a new component. This component should produce some html
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. This component should produce
+// some HTML
+
+const App = () => {
+  return <div>Hi</div>;
+}
+
+// Take this component's genreated HTLM an dput it
+// on the page (in the DOM)
+ReactDOM.render(<App />
+  ,
+  document.querySelector('.container')
+);
