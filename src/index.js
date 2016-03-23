46d05798e502cd149dde5b2aada97b92dc78c906
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-22 21:48:19 -0700

    adds VideoList component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDXrY5YCNGwaUD19FvtiXLCn_BCb0qT5IM';
 
@@ -13,13 +14,14 @@ class App extends Component {
     this.state = { videos: [] };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-      this.setState({ vidoes });
+      this.setState({ videos });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
