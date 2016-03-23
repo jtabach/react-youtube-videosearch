0d99b903e6490f7b5a9a87431799a145b027f316
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-22 22:21:59 -0700

    renders iframe of video to video detail

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyDXrY5YCNGwaUD19FvtiXLCn_BCb0qT5IM';
 
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
